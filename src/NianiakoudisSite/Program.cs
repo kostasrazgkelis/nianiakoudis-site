43142bb26a9e1640df55a9f3e9fff24dad17d509
Konstantinos Razgkel <k.razgkelis@outlook.com>
2026-01-23 00:16:49 +0200

    Add localization support and Greek translations
    - Implemented localization in the Program.cs file, setting the default culture to Greek (el-GR).
    - Created a new resource file (SharedResource.resx) containing Greek translations for various UI elements and messages.
    - Updated the PageContentService to utilize the localization service for fetching localized strings.
    - Modified MainLayout.razor and NavMenu.razor to display localized text for navigation and footer elements.
    - Added a SharedResource class to serve as a placeholder for localization.
    - Updated _Imports.razor to include necessary namespaces for localization.

--- src/NianiakoudisSite/Program.cs
+++ src/NianiakoudisSite/Program.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -10,6 +11,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<IPageContentService, PageContentService>();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddMudServices();
+
+var culture = new CultureInfo("el-GR");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 await builder.Build().RunAsync();
