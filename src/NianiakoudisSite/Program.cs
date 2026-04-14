c513e73fa9230561574cf1399e6e006bde9f826e
konstantinos.razgkelis.external <konstantinos.razgkelis.external@tkelevetaor.com>
2026-04-14 13:16:02 +0300

    feat: add necessary using directives for Google Analytics and localization

--- src/NianiakoudisSite/Program.cs
+++ src/NianiakoudisSite/Program.cs
@@ -1,3 +1,11 @@
+using System.Globalization;
+using Blazor.Analytics;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using NianiakoudisSite;
+using NianiakoudisSite.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
