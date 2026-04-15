824dd27ef4af7c9f91b2f7c4e088fa3266974bce
konstantinos.razgkelis.external <konstantinos.razgkelis.external@tkelevetaor.com>
2026-04-15 11:54:54 +0300

    feat: revert Google Analytics tracking ID to previous version

--- src/NianiakoudisSite/Program.cs
+++ src/NianiakoudisSite/Program.cs
@@ -14,9 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<IPageContentService, PageContentService>();
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddMudServices();
-// production
-//builder.Services.AddGoogleAnalytics("G-R36FHVREES");
-builder.Services.AddGoogleAnalytics("G-J12Z3G3G6E");
+builder.Services.AddGoogleAnalytics("G-R36FHVREES");
 
 var culture = new CultureInfo("el-GR");
 CultureInfo.DefaultThreadCurrentCulture = culture;
